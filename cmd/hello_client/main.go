6d0d2e6fbb5195cbe7b8d350aa23a2dad3830443
Boris Ershov <borisershov@www.mts.ru>
2022-07-25 12:49:24 +0300

    grpc lb RND

--- cmd/hello_client/main.go
+++ cmd/hello_client/main.go
@@ -4,15 +4,20 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"sync"
 	"time"
 
 	h "github.com/WWtLF/hello_grpc/pkg/api/hello"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/resolver"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	resolver.SetDefaultScheme("dns")
+	conn, err := grpc.Dial(os.Getenv("SERVER_HOST_PORT"), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -22,30 +27,35 @@ func main() {
 	// Contact the server and print out its response.
 
 	// defer cancel()
-	for i := 0; i < 1000; i++ {
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
 		time.Sleep(time.Second)
 		// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		client, err := c.SayHello(context.Background(), &h.Test{Test: fmt.Sprintf("request number %d", i)})
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, gr_num int) {
+			fmt.Println("Starting reqiuest goroutin ", gr_num)
+			client, err := c.SayHello(context.Background(), &h.Test{Test: fmt.Sprintf("request number %d", gr_num)})
 
-		if err != nil {
-			fmt.Printf("could not ping: %v\n\r", err)
-			continue
-		}
-
-		for {
-			test, recvErr := client.Recv()
-			if recvErr != nil {
-				fmt.Println("Error ", recvErr.Error())
-				client.CloseSend()
-				break
-			}
-			if test != nil {
-				fmt.Println(test.Test)
+			if err != nil {
+				fmt.Printf("could not ping: %v\n\r", err)
+				return
 			}
 
-		}
+			for {
+				test, recvErr := client.Recv()
+				if recvErr != nil {
+					fmt.Println("Error ", recvErr.Error())
+					client.CloseSend()
+					break
+				}
+				if test != nil {
+					fmt.Println(test.Test)
+				}
+
+			}
+			wg.Done()
+		}(&wg, i)
 		// cancel()
-
 	}
-
+	wg.Wait()
 }
--- makefile
+++ makefile
@@ -8,3 +8,6 @@ proto:
     --go-grpc_out=./pkg --go-grpc_opt=paths=source_relative \
     api/hello/*.proto
 
+docker:
+	docker build -t hello_client -f deployment/hello_client/Dockerfile .
+	docker build -t hello_server -f deployment/hello_server/Dockerfile .
