b74e700b8b6aa90c601500448ea5e25c632e508e
Boris Ershov <boris109@gmail.com>
2022-07-30 13:15:39 +0300

    intercepter

--- cmd/hello_client/main.go
+++ cmd/hello_client/main.go
@@ -9,15 +9,34 @@ import (
 	"time"
 
 	h "github.com/WWtLF/hello_grpc/pkg/api/hello"
+	"github.com/WWtLF/hello_grpc/pkg/tracing"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/resolver"
 )
 
 func main() {
+	ctx := context.Background()
+	tp, err := tracing.TracerProvider("http://jaeger-all-in-one:14268/api/traces")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() { _ = tp.Shutdown(ctx) }()
+
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	// tr := tp.Tracer("component-main")
 
 	resolver.SetDefaultScheme("dns")
-	conn, err := grpc.Dial(os.Getenv("SERVER_HOST_PORT"), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	conn, err := grpc.Dial(os.Getenv("SERVER_HOST_PORT"),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+		grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor()),
+		grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -34,7 +53,15 @@ func main() {
 		wg.Add(1)
 		go func(wg *sync.WaitGroup, gr_num int) {
 			fmt.Println("Starting reqiuest goroutin ", gr_num)
-			client, err := c.SayHello(context.Background(), &h.Test{Test: fmt.Sprintf("request number %d", gr_num)})
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			// ctx, span := tr.Start(ctx, "SayHello")
+			// defer span.End()
+			client, err := c.SayHello(ctx, &h.Test{Test: fmt.Sprintf("request number %d", gr_num)})
+			// span.SetAttributes(attribute.KeyValue{
+			// 	Key:   "RequestID",
+			// 	Value: attribute.Int64Value(int64(gr_num)),
+			// })
 
 			if err != nil {
 				fmt.Printf("could not ping: %v\n\r", err)
--- cmd/hello_server/main.go
+++ cmd/hello_server/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
-	"time"
 
 	h "github.com/WWtLF/hello_grpc/pkg/api/hello"
+	"github.com/WWtLF/hello_grpc/pkg/tracing"
 	"github.com/google/uuid"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
 	"google.golang.org/grpc"
 )
 
@@ -18,36 +22,58 @@ var (
 
 type PingServer struct {
 	h.UnimplementedPingServer
+	// tr trace.Tracer
 }
 
 func (s *PingServer) SayHello(test *h.Test, p h.Ping_SayHelloServer) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	// ctx, span := s.tr.Start(ctx, "SayHello_server")
+	// defer span.End()
+
 	fmt.Println("NEW CONNECTION")
-	send(test.Test, my_uuid, p)
+	s.send(ctx, test.Test, my_uuid, p)
 	fmt.Println("Stream finished")
 	return nil
 }
 
-func send(req string, text string, p h.Ping_SayHelloServer) {
+func (s *PingServer) send(ctx context.Context, req string, text string, p h.Ping_SayHelloServer) {
 	for i := 0; i < 100; i++ {
+		// _, span := s.tr.Start(ctx, "SayHello_SEND")
+		// span.End()
 		fmt.Printf("Request %s, I send %s: %d \n\r", req, text, i)
 		err := p.Send(&h.Test{Test: fmt.Sprintf("%s: %d", text, i)})
 		if err != nil {
 			fmt.Println("ERROR FINISHED ", err.Error())
 			return
 		}
-		time.Sleep(time.Second)
+		// time.Sleep(time.Second)
+		// span.End()
 	}
 	fmt.Println("NORMAL FINISHED")
 }
 
 func main() {
+	ctx := context.Background()
+	tp, err := tracing.TracerProvider("http://jaeger-all-in-one:14268/api/traces")
+	defer func() { _ = tp.Shutdown(ctx) }()
+	if err != nil {
+		log.Fatal(err)
+	}
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	// tr := tp.Tracer("component-main")
 
 	fmt.Println("My POD IP is ", os.Getenv("POD_IP"))
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50051))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor()),
+		grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()),
+	)
+	// h.RegisterPingServer(s, &PingServer{tr: tr})
 	h.RegisterPingServer(s, &PingServer{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
